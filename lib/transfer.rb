b214058cf1abb02a8b262953976918d082af462f
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-25 21:03:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       @status = "complete"
     else 
       @status = "rejected"
-      "Transaction #{status}. Please check your account balance."
+      "Transaction #{@status}. Please check your account balance."
     end 
   end
   
